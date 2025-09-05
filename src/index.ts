83192b0ec0489d4ae662d7bafbad967dbbfce04a
Juliette Wang immjs <immjs@immjs.dev>
2025-09-05 16:55:20 +0200

    fix: Remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { interfaces } from "./wayland_interpreter.js";
-
 export * from "./compositor.js";
 export * from "./connection.js";
 export * from "./wayland_interpreter.js";
 export * from "./utils.js";
-
-console.log(interfaces.wl_buffer);
